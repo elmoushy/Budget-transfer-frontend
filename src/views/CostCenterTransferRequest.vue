2aac0dc06b6d2a48e3c08cfcb130158b2d9c90b9
Ahmed <ahmed@192.168.1.5>
2025-09-10 12:51:14 +0300

    handel project name

--- src/views/CostCenterTransferRequest.vue
+++ src/views/CostCenterTransferRequest.vue
@@ -1310,6 +1310,7 @@ const createTransfer = async () => {
         account_code: item.account_code,
         account_name: item.account_name,
         project_code: item.project_code,
+        project_name: item.project_name,
         approved_budget: toSafeNumber(item.approved_budget),
         available_budget: toSafeNumber(item.available_budget),
         to_center: toSafeNumber(item.to_center),
